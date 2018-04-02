55dd31501bd99ac01132ac9c9819b3d3e79b6d51
Kaspar Suvi <kaspar.suvi@iglu.ee>
2018-04-02 10:06:49 +0300

    One more try to fix

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 const proxy = require('express-http-proxy');
 
+//redirect
+app.use('/oauth/token', proxy('http://138.68.71.15:8080'));
+app.use('/api', proxy('http://138.68.71.15:8080'));
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-//redirect
-app.use('/oauth/token', proxy('http://138.68.71.15:8080'));
-app.use('/api', proxy('http://138.68.71.15:8080'));
